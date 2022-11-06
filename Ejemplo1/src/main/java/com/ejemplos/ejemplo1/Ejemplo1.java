f27da5f4531b9f1d2392bbb8fb106f6afbc1526f
Valeria Botero Durango <valboterod@gmail.com>
2022-11-06 17:17:36 -0500

    Ejemplo 1 de Netbeans

---
+++ Ejemplo1/src/main/java/com/ejemplos/ejemplo1/Ejemplo1.java
@@ -0,0 +1,24 @@
+// Paquete de datos: son como las carpetas donde se guardan las clases
+/*
+para la encapsulacion de las clases, se deben tener como priorodad los paquetes, su respectiva importacion si depende de otra carpeta
+
+*/
+
+package com.ejemplos.ejemplo1;
+
+//las clase es la caracteristica general o global de los objetos por crear
+public class Ejemplo1 {
+/*
+    public: es el acceso del metodo, tambien esta el acceso private, protected
+    metodo: es una accion que se ejecuta en tiempo real 
+    static: describe el comportamiento del metodo 
+    void: vacio-- no retorna ningun tipo de dato, o retorna vacio
+    main: es el metodo principal 
+    (string[] args) son los parametros de dicho metodo
+    */
+    public static void main(String[] args) {
+        // la forma de mostrar datos por consola
+        System.out.println("Hello World!");
+        System.out.println("Hola tripulantes");
+    }
+}
